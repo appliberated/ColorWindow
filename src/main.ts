77f37a608dcfddc42863235d8f7dfe3967df4bb6
helloworldwriter <writer@helloworldwriter.com>
2020-08-13 12:07:56 +0300

    Add buttons and style

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,39 @@
 import './components/appl-color-typer.js';
 
+const mainElem = document.getElementById('main');
+
 const colorTyperElem = document.querySelector('appl-color-typer');
 colorTyperElem?.addEventListener('colorchange', (e) => {
   document.body.style.backgroundColor = e.detail.color;
 });
 
+
+function initHideOverlay() {
+  const hideButton = document.getElementById('hide-overlay');
+  hideButton?.addEventListener('click', () => {
+    mainElem?.hidden = true;
+  });
+}
+
+
+function initToggleFullScreen() {
+  const fullScreenButton = document.getElementById('toggle-fullscreen');
+  fullScreenButton?.addEventListener("click", () => {
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen();
+    } else {
+      document.exitFullscreen();
+    }
+  });
+}
+
+function initColorTyper() {
+  const colorTyperInput = document.getElementById('color-typer');
+  colorTyperInput?.addEventListener('input', (e) => {
+    document.body.style.backgroundColor = e.target.value;
+  });
+}
+
+initColorTyper();
+initToggleFullScreen();
+initHideOverlay();
